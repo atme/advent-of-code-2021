598a181a4a4cf0c5671f1e781d9ee39c7a03a0bc
Serge Pirogov <d3ljke@gmail.com>
2021-12-13 14:02:44 +0100

    Refactor day 13

--- src/day13.ts
+++ src/day13.ts
@@ -26,13 +26,12 @@ export const fold = (sheet: Sheet, instruction: Instruction) => {
     .filter((dot) => dot[axis] > coordinate)
     .map((dot) => ({ ...dot, [axis]: doubleCoordinate - dot[axis] }))
 
-  return [
-    ...sheet.filter((dot) => dot[axis] < coordinate),
-    ...foldedDots,
-  ].filter(
-    (dot, index, dots) =>
-      dots.findIndex(({ x, y }) => dot.x === x && dot.y === y) === index
-  )
+  return [...sheet, ...foldedDots]
+    .filter((dot) => dot[axis] < coordinate)
+    .filter(
+      (dot, index, dots) =>
+        dots.findIndex(({ x, y }) => dot.x === x && dot.y === y) === index
+    )
 }
 
 export const first = (input: string) => {
@@ -44,22 +43,16 @@ const draw = (sheet: Sheet) => {
   const maxX = Math.max(...sheet.map((dot) => dot.x)) + 1
   const maxY = Math.max(...sheet.map((dot) => dot.y)) + 1
 
-  const canvas = range(maxY).map((_) => range(maxX).fill(0))
+  const canvas = range(maxY).map((_) => range(maxX).map((_) => '.'))
   sheet.forEach(({ x, y }) => {
-    canvas[y][x] = 1
+    canvas[y][x] = '#'
   })
 
-  return canvas
-    .map((line) => line.map((dot) => (dot === 1 ? '#' : '.')).join(''))
-    .join('\n')
+  return canvas.map((line) => line.join('')).join('\n')
 }
 
 export const second = (input: string) => {
   const { sheet, instructions } = parse(input)
-  const finalSheet = instructions.reduce(
-    (dots, instruction) => fold(dots, instruction),
-    sheet
-  )
-
+  const finalSheet = instructions.reduce(fold, sheet)
   return draw(finalSheet)
 }
